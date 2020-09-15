81a42a155e9217763ea7f1957c69ab8908ea5fdd
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2020-09-15 12:55:56 -0300

    exercício 2 - funcionários

---
+++ Exercicios_24-08/2_Funcionarios/Funcionario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace POO___Aula_1___Ex3
+{
+    class Funcionario
+    {
+        public string nome;
+        public float salario;
+
+        public double SalarioMedia()
+        {
+            double salario_media = (salario) / 2;
+            return salario_media;
+        }
+    }
+}
+
---
+++ Exercicios_24-08/2_Funcionarios/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace POO___Aula_1___Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario a, b;
+            a = new Funcionario();
+            b = new Funcionario();
+
+            Console.Write("Digite o nome do primeiro funcionário: ");
+            a.nome = Console.ReadLine();
+            Console.Write("Digite a salário do primeiro funcionário: ");
+            a.salario = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite o nome do segundo funcionário: ");
+            b.nome = Console.ReadLine();
+            Console.Write("Digite o salário do segundo funcionário: ");
+            b.salario = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n\nDados do primeiro funcionário:\nNome: " + a.nome + "\nSalário: R$" + a.salario.ToString("F2") + "\n");
+            Console.WriteLine("Dados do segundo funcionário:\nNome: " + b.nome + "\nSalário: R$" + b.salario.ToString("F2") + "\n");
+
+            //float salario_media = (a.salario + b.salario) / 2;
+
+            float c = a.salario + b.salario;
+
+            Console.WriteLine("Salário médio: R$" + salario_media.ToString("F2"));
+        }
+    }
+}
