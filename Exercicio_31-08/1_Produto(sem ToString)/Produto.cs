e36f00bdca3633c90a2875dfe9f80165337e62d1
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2020-09-15 13:26:12 -0300

    exercício (31/08) - produto (sem ToString)

---
+++ Exercicio_31-08/1_Produto(sem ToString)/Produto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace POO_31_8_Ex1
+{
+	public class Produto
+	{
+		public string Nome;
+		public double Preco;
+		public int Quantidade;
+
+		public double ValorTotalEstoque()
+        {
+			double total = Quantidade * Preco;
+			return total;
+        }
+
+		public void AdicionarProdutos(int qnt)
+        {
+			Quantidade += qnt;
+        }
+
+		public void RemoverProdutos(int qnt)
+        {
+			Quantidade -= qnt;
+		}
+	}
+}
+
---
+++ Exercicio_31-08/1_Produto(sem ToString)/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+namespace POO_31_8_Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+
+            Console.Write("Digite o nome do produto: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Digite o preço do produto em reais: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Digite a quantidade em estoque do produto: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine($"\nDados do produto " +
+                $"\nNome: {p.Nome}" +
+                $"\nPreço: R${p.Preco.ToString("F2", CultureInfo.InvariantCulture)}" +
+                $"\n{p.Quantidade.ToString(CultureInfo.InvariantCulture)} unidades" +
+                $"\nTotal: R${p.ValorTotalEstoque().ToString("F2", CultureInfo.InvariantCulture)}\n");
+
+
+            Console.Write("Digite o valor a ser adicionado ao estoque: ");
+            p.AdicionarProdutos(int.Parse(Console.ReadLine()));
+
+
+            Console.WriteLine($"\nDados atualizados do produto " +
+                $"\nNome: {p.Nome}" +
+                $"\nPreço: R${p.Preco.ToString("F2", CultureInfo.InvariantCulture)}" +
+                $"\n{p.Quantidade.ToString(CultureInfo.InvariantCulture)} unidades" +
+                $"\nTotal: R${p.ValorTotalEstoque().ToString("F2", CultureInfo.InvariantCulture)}\n");
+
+
+            Console.Write("Digite o valor a ser removido ao estoque: ");
+            p.RemoverProdutos(int.Parse(Console.ReadLine()));
+
+
+            Console.WriteLine($"\nDados atualizados do produto " +
+                $"\nNome: {p.Nome}" +
+                $"\nPreço: R${p.Preco.ToString("F2", CultureInfo.InvariantCulture)}" +
+                $"\n{p.Quantidade.ToString(CultureInfo.InvariantCulture)} unidades" +
+                $"\nTotal: R${p.ValorTotalEstoque().ToString("F2", CultureInfo.InvariantCulture)}\n");
+        }
+    }
+}
