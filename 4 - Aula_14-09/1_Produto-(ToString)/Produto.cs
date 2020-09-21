9cb7bd6a4d11860f7efb4eca3f83df98770ba79b
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2020-09-21 14:20:01 -0300

    aula 14-09 + organização de pastas

---
+++ 4 - Aula_14-09/1_Produto-(ToString)/Produto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1_Produto__ToString_
+{
+	public class Produto
+	{
+		public string Nome;
+		public double Preco;
+		public int Quantidade;
+
+		public double ValorTotalEstoque()
+		{
+			double total = Quantidade * Preco;
+			return total;
+		}
+
+		public void AdicionarProdutos(int qnt)
+		{
+			Quantidade += qnt;
+		}
+
+		public void RemoverProdutos(int qnt)
+		{
+			Quantidade -= qnt;
+		}
+
+
+		public override string ToString()
+		{
+			return "Nome: " + Nome + "\n"
+				+ "Preço: R$ " + Preco.ToString("F2", CultureInfo.InvariantCulture) + "\n"
+				+ Quantidade + " unidades em estoque\n"
+				+ "Total: R$ " + ValorTotalEstoque().ToString("F2", CultureInfo.InvariantCulture);
+		}
+	}
+}
---
+++ 4 - Aula_14-09/1_Produto-(ToString)/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1_Produto__ToString_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+
+            Console.Write("Digite o nome do produto: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Digite o preço do produto em reais: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Digite a quantidade em estoque do produto: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("\nDados do produto \n" + p + "\n\n");
+
+
+            Console.Write("Digite o valor a ser adicionado ao estoque: ");
+            p.AdicionarProdutos(int.Parse(Console.ReadLine()));
+
+
+            Console.WriteLine($"\nDados atualizados do produto \n" + p + "\n\n");
+
+
+            Console.Write("Digite o valor a ser removido ao estoque: ");
+            p.RemoverProdutos(int.Parse(Console.ReadLine()));
+
+
+            Console.WriteLine($"\nDados atualizados do produto \n" + p + "\n\n");
+
+            Console.ReadKey();
+        }
+    }
+}
