19118045348a736b87880dba30f8fbe4043a54f6
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2020-10-20 18:12:56 -0300

    exercício construtores

--- 5 - Aula_21-09/1_Produto-(Construtores)/Produto.cs
+++ 5 - Aula_21-09/1_Produto-(Construtores)/Produto.cs
@@ -5,10 +5,12 @@ namespace _1_Produto__Construtores_
 {
 	public class Produto
 	{
+		//variáveis públicas principais
 		public string Nome;
 		public double Preco;
 		public int Quantidade;
 
+		//construtor de classe que recebe parâmetros que serão atribuidos nas variáveis principais, e garantem o não recebimento de dados nulos na criação de um objeto
 		public Produto (string nome, double preco, int quantidade)
 		{
 			Nome = nome;
@@ -16,12 +18,14 @@ namespace _1_Produto__Construtores_
 			Quantidade = quantidade;
 		}
 
+		//método pra calcular o valor total do estoque
 		public double ValorTotalEstoque()
 		{
 			double total = Quantidade * Preco;
 			return total;
 		}
 
+		//métodos pra modificar a quantidade de produtos
 		public void AdicionarProdutos(int qnt)
 		{
 			Quantidade += qnt;
@@ -32,7 +36,7 @@ namespace _1_Produto__Construtores_
 			Quantidade -= qnt;
 		}
 
-
+		//método ToString pra ter uma formatação adequada quando precisar mostrar a informação
 		public override string ToString()
 		{
 			return "Nome: " + Nome + "\n"
--- 5 - Aula_21-09/1_Produto-(Construtores)/Program.cs
+++ 5 - Aula_21-09/1_Produto-(Construtores)/Program.cs
@@ -7,15 +7,7 @@ namespace _1_Produto__Construtores_
     {
         static void Main(string[] args)
         {
-            //Produto p = new Produto();
-
-            /*Console.Write("Digite o nome do produto: ");
-            p.Nome = Console.ReadLine();
-            Console.Write("Digite o preço do produto em reais: ");
-            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.Write("Digite a quantidade em estoque do produto: ");
-            p.Quantidade = int.Parse(Console.ReadLine());*/
-
+            //entrada do usuário de dados que serão guardados em variáveis privadas inicialmente
             Console.Write("Digite o nome do produto: ");
             string nome = Console.ReadLine();
             Console.Write("Digite o preço do produto em reais: ");
@@ -23,22 +15,24 @@ namespace _1_Produto__Construtores_
             Console.Write("Digite a quantidade em estoque do produto: ");
             int quantidade = int.Parse(Console.ReadLine());
 
+            //instância de um objeto p da classe Produto já utilizando as variáveis privadas como parâmetros
             Produto p = new Produto(nome, preco, quantidade);
 
+            //mostra de informações do objeto formatadas pelo ToString
             Console.WriteLine("\nDados do produto \n" + p + "\n\n");
 
-
+            //método de adicionar produtos utilizado
             Console.Write("Digite o valor a ser adicionado ao estoque: ");
             p.AdicionarProdutos(int.Parse(Console.ReadLine()));
 
-
+            //mostra de informações do objeto formatadas pelo ToString
             Console.WriteLine($"\nDados atualizados do produto \n" + p + "\n\n");
 
-
+            //método de remover produtos utilizado
             Console.Write("Digite o valor a ser removido ao estoque: ");
             p.RemoverProdutos(int.Parse(Console.ReadLine()));
 
-
+            //mostra de informações do objeto formatadas pelo ToString
             Console.WriteLine($"\nDados atualizados do produto \n" + p + "\n\n");
 
             Console.ReadKey();
