a0108b81d5eed3db05d195887da5cf7305dffcb1
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2020-11-10 21:29:38 -0300

    comentários do exercício ContaBancaria

--- 7 - Aula_05-10/1_ContaBancaria/ContaBancaria.cs
+++ 7 - Aula_05-10/1_ContaBancaria/ContaBancaria.cs
@@ -5,32 +5,24 @@ namespace _1_ContaBancaria
 {
     class ContaBancaria
     {
-        int _numero;
-        string _titular;
+        //declaração de variáveis com autoproperties
+        public int Numero { get; set; }
+        public string Titular { get; set; }
         public double Saldo { get; private set; }
 
+        //construtor padrão
         public ContaBancaria()
         {
         }
 
+        //construtor personalizado
         public ContaBancaria(int numero, string titular)
         {
-            _numero = numero;
-            _titular = titular;
+            Numero = numero;
+            Titular = titular;
         }
 
-        public int Numero
-        {
-            get { return _numero; }
-            set { _numero = value; }
-        }
-
-        public string Titular
-        {
-            get { return _titular; }
-            set { _titular = value; }
-        }
-
+        //métodos de classe pra depositos e saques
         public void RealizarDeposito(double valor)
         {
             Saldo += valor;
@@ -43,8 +35,8 @@ namespace _1_ContaBancaria
 
         public override string ToString()
         {
-            return  "\nConta   - " + _numero.ToString(CultureInfo.InvariantCulture)
-                    + "\nTitular - " + _titular
+            return  "\nConta   - " + Numero.ToString(CultureInfo.InvariantCulture)
+                    + "\nTitular - " + Titular
                     + "\nSaldo   - R$ " + Saldo.ToString("F2", CultureInfo.InvariantCulture);
         }
     }
--- 7 - Aula_05-10/1_ContaBancaria/Program.cs
+++ 7 - Aula_05-10/1_ContaBancaria/Program.cs
@@ -6,33 +6,42 @@ namespace _1_ContaBancaria
     {
         static void Main(string[] args)
         {
+            //criação de um objeto contabancaria vazio
             ContaBancaria a = new ContaBancaria();
 
+            //condição para o número do objeto ser definido só na primeira vez
             if (a.Numero == default)
             {
+                //input do usuário para o numero da conta que será setado na variável Numero
                 Console.Write("Entre com o número da conta: ");
                 a.Numero = int.Parse(Console.ReadLine());
             }
 
+            //input do usuário para o nome do titular da conta que será setado na variável Titular
             Console.Write("Entre com o titular da conta: ");
             a.Titular = Console.ReadLine();
 
+            //checagem para a realização do depósito inicial
             Console.Write("Haverá depósito inicial? (S/N): ");
             string depositoInicial = Console.ReadLine();
 
+            //se o usuário digitar S, ele poderá realizar o depósito inicial da conta
             if (depositoInicial == "S")
             {
+                //como só é possível alterar o saldo dentro da classe contabancaria, o valor digitado pelo usuário será aplicado no método realizardeposito
                 Console.Write("Entre com o depósito inicial: ");
                 a.RealizarDeposito(double.Parse(Console.ReadLine()));
             }
 
+            //se o usuário digitar algo além de S, não haverá depósito inicial e o programa realiza um depósito com valor 0 para aplicar um valor ao saldo
             else
                 a.RealizarDeposito(0);
 
+            //output dos dados da conta
             Console.WriteLine("\nDados da conta:" + a);
 
 
-
+            //demonstração de depósito e saque utilizando os métodos da classe contabancaria e outputs atualizados dos dados da conta
             Console.Write("\nEntre com um valor para depósito: ");
             a.RealizarDeposito(double.Parse(Console.ReadLine()));
 
