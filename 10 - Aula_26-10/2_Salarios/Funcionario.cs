26637449864a3199592642637157cdf9a470ce8b
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2020-11-23 01:28:15 -0300

    exercício salarios

--- 10 - Aula_26-10/2_Salarios/Funcionario.cs
+++ 10 - Aula_26-10/2_Salarios/Funcionario.cs
@@ -7,37 +7,47 @@ namespace _2_Salarios
 {
     class Funcionario
     {
+        //declaração de variáveis, id com propriedades autoimplementadas
         public int Id { get; private set; }
         private string _nome;
         private double _salario;
 
+        //construtor padrão
         public Funcionario()
         {
         }
 
+        //construtor personalizado que atribui os valores para Id e _nome
         public Funcionario(int id, string nome)
         {
             Id = id;
             _nome = nome;
         }
 
+        //variável publica Salario com propriedades customizadas
         public double Salario
         {
             get { return _salario; }
 
-            set { if (_salario == default)
+            //se o valor de Salário do objeto for o padrão, o usuário modificará-o somente na primeira vez
+            //quaisquer modificações futuras serão feitas por meio de métodos
+            set
+            { if (_salario == default)
                     {
                         _salario = value;
                     } 
             }
         }
 
+        //método público para aumentar o salário
+        //ele multiplica o valor atual do salário por 100 + (porcentagem digitada pelo usuário), tudo dividido por 100 para chegar no decimal correto
+        //ex: aumento de 40% = (100 + 40)/100 = 1.4
         public void AumentarSalario(double porcentagem)
         {
             _salario *= (100 + porcentagem) / 100;
         }
 
-
+        //método tostring para o programa mostrar a formatação correta na hora de exibir os objetos da classe Funcionario
         public override string ToString()
         {
             return Id.ToString("000")
--- 10 - Aula_26-10/2_Salarios/Program.cs
+++ 10 - Aula_26-10/2_Salarios/Program.cs
@@ -8,13 +8,17 @@ namespace _2_Salarios
     {
         static void Main(string[] args)
         {
+            //o usuário digita o número de funcionários que serão registrados e salvos na variável N
             Console.Write("Quantos funcionários serão registrados? ");
             int N = int.Parse(Console.ReadLine());
 
+            //criação de uma lista que armazenará objetos da classe Funcionario
             List<Funcionario> lista = new List<Funcionario>();
 
+            //loop que repete o número de vezes que o usuário digitou para a quantidade de funcionários
             for (int i = 0; i < N; i++)
             {
+                //toda iteração do loop o usuário digitará os valores para a Id, Nome e Salário
                 Console.WriteLine("\nFuncionário #" + (i + 1) + ":");
 
                 Console.Write("Id: ");
@@ -26,35 +30,47 @@ namespace _2_Salarios
                 Console.Write("Salário: ");
                 double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
+                //depois de ler os valores, um objeto Funcionario é criado e inserido na lista na posição de número atual do loop (iniciando em 0)
+                //o objeto é criado primeiramente com os valores de id e nome
                 lista.Insert(i, new Funcionario(id, nome));
+                //mas em seguida a variável Salario do objeto atual é definida também
                 lista[i].Salario = salario;
             }
 
+            //input do usuário para o id do funcionário que receberá aumento que será salva na string idAumento
             Console.Write("\nId do funcionário que receberá aumento: ");
             string idAumento = Console.ReadLine();
 
-
+            //como o id estará formatado no padrão 000, uma segunda lista vai ser criada e preenchida com os objetos da primeira lista que terão o id digitado pelo usuário
+            //eu tava tendo problemas pq os ids iniciais estavam sendo digitados como 001, mas estavam sendo armazenados na classe como 1 por serem variáveis int
+            //então se a input digitada na idAumento fosse "001", o findall não iria achar um objeto correspondente, pois a id dele seria somente 1
+            //então só converti o Id da classe para 000, aí não teve problemas
             List<Funcionario> lista2 = lista.FindAll(x => x.Id.ToString("000") == idAumento);
 
-
+            //se nenhum resultado for encontrado, output de "funcionário não existe"
             if (lista2 == null)
             {
                 Console.WriteLine("O funcionário de ID " + lista2[0].Id.ToString("000") + " não existe!");
-                //Console.WriteLine("Este funcionário não existe!");
                 Console.ReadKey();
             }
 
+            //se o funcionário de tal id existir, realizar as coisas abaixo
             else
             {
+                //input para a porcentagem desejada de aumento para o funcionário de índice 0 da lista2, que só tem o funcionário com a id desejada
                 Console.Write("Porcentagem de aumento de salário: ");
                 lista2[0].AumentarSalario(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
 
+                //mostrar todos os funcionários
                 Console.WriteLine("\nLista atualizada de funcionários da Bravo: ");
+                //para cada Funcionario em lista, mostrar o objeto que já terá a formatação correta
                 foreach (Funcionario obj in lista)
                 {
                     Console.WriteLine(obj);
                 }
             }
+
+            Console.ReadKey();
         }
     }
 }
